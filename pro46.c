7a040f1b0970814627a1d3ecd27327ede218e114
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-02-06 14:54:07 +0530

    Create pro46.c

---
+++ pro46.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int n;float ang;
+printf("enter the angle");
+scanf("%d",&n);
+ang=sin(n);
+printf("sine of the angle is %.4f",ang);
+return 0;
+}
